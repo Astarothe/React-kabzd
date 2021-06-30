88ae145006b3bb9afda6dfdb167357f33d66b31b
Astaroth <minatosavich@gmail.com>
2021-06-30 15:03:46 +0300

    feat: add useMemo hook

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {Story} from '@storybook/react';
 import {Select, SelectPropsType} from "./Select";
 import {action} from "@storybook/addon-actions";
+import s from "./Select.module.css";
 
 export default {
     title: "Select",
@@ -14,38 +15,107 @@ const Template: Story<SelectPropsType> = (args) => {
     )
 }
 
-export const WithValue = () => {
-    const [value, setValue] = useState("2");
-    const onChange = (val:string) => setValue(val)
+// export const WithValue = () => {
+//     const [value, setValue] = useState("2");
+//     const onChange = (val: string) => setValue(val)
+//
+//     return (
+//         <Select onChange={onChange}
+//                 value={value}
+//                 items={[
+//                     {title: "Minsk", value: "1"},
+//                     {title: "Moscow", value: "2"},
+//                     {title: "Kiev", value: "3"},
+//                 ]}/>
+//     )
+// };
+
+
+// export const WithoutValue = () => {
+//     const [value, setValue] = useState(null);
+//     const onChange = (val: any) => setValue(val)
+//
+//     return (
+//         <Select onChange={onChange}
+//                 value={value}
+//                 items={[
+//                     {title: "Minsk", value: "1"},
+//                     {title: "Moscow", value: "2"},
+//                     {title: "Kiev", value: "3"},
+//                 ]}/>
+//     )
+// }
+
+export const Example = () => {
+
+    const select = [
+        {id: 1, filter: "string"},
+        {id: 2, filter: "country"},
+        {id: 3, filter: "count"},
+    ]
+    const [title, setTitle] = useState({
+        [select[0].id]:
+            [{title: "Minsk", value: "1", country: 1, count: 1000},
+                {title: "Polotck", value: "2", country: 1, count: 100000},
+                {title: "Novopolotck", value: "3", country: 1, count: 1000},
+                {title: "Moscow", value: "4", country: 2, count: 1000},
+                {title: "Kiev", value: "5", country: 2, count: 100000},
+                {title: "Peterburg", value: "6", country: 2, count: 100000},
+            ],
+        [select[1].id]:
+            [{title: "Minsk", value: "1", country: 1, count: 1000},
+                {title: "Polotck", value: "2", country: 1, count: 100000},
+                {title: "Novopolotck", value: "3", country: 1, count: 1000},
+                {title: "Moscow", value: "4", country: 2, count: 1000},
+                {title: "Kiev", value: "5", country: 2, count: 100000},
+                {title: "Peterburg", value: "6", country: 2, count: 100000},
+            ],
+        [select[2].id]:
+            [{title: "Minsk", value: "1", country: 1, count: 1000},
+                {title: "Polotck", value: "2", country: 1, count: 100000},
+                {title: "Novopolotck", value: "3", country: 1, count: 1000},
+                {title: "Moscow", value: "4", country: 2, count: 1000},
+                {title: "Kiev", value: "5", country: 2, count: 100000},
+                {title: "Peterburg", value: "6", country: 2, count: 100000},
+            ]
+    })
+
 
     return (
-        <Select onChange={onChange}
-            value={value}
-                items={[
-                    {title: "Minsk", value: "1"},
-                    {title: "Moscow", value: "2"},
-                    {title: "Kiev", value: "3"},
-                ]}/>
-    )
-};
+        <div className={s.wrapper}>
+            {select.map(t => {
+                let newTitle = title[1];
+                if (t.filter === "string") {
+                    newTitle = title[t.id].filter(t => t.title.indexOf("o") > -1)
+                    console.log(newTitle)
+                }
+                if (t.filter === "country") {
+                    newTitle = title[t.id].filter(t => t.country < 2)
+                }
+                if (t.filter === "count") {
+                    newTitle = title[t.id].filter(t => t.count > 1000)
+                }
+
+                return <Select
+                    key={t.id}
+                    items={newTitle}
+                    filter={t.filter}/>
+            })}
 
 
-export const WithoutValue = () => {
-    const [value, setValue] = useState(null);
-    const onChange = (val:any ) => setValue(val)
+        </div>
 
-    return (
-        <Select onChange={onChange}
-                value={value}
-                items={[
-                    {title: "Minsk", value: "1"},
-                    {title: "Moscow", value: "2"},
-                    {title: "Kiev", value: "3"},
-                ]}/>
     )
 }
 
-
+// <Select onChange={onChange}
+//         value={value}
+//         items={title}
+//         filter={"country"}/>
+// <Select onChange={onChange}
+//         value={value}
+//         items={title}
+//         filter={"count"}/>
 // const Selected = Template.bind({});
 //
 // Selected.args = {
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -3,29 +3,37 @@ import s from "./Select.module.css"
 
 export type ItemType = {
     title: string
-    value: any
+    value: string
+    country: number
+    count: number
+
 }
 
 export type SelectPropsType = {
-    value: any
-    onChange: (value: any) => any
-    items: ItemType[]
+    items: Array<ItemType>
+    filter: string
 }
 
 export function Select(props: SelectPropsType) {
-    const [active, setActive] = useState(false);
-    const [hoveredElementValue, setHoveredElementValue] = useState(props.value);
+    const [value, setValue] = useState(null);
+    const onChange = (val: any) => setValue(val)
+    console.log(value + " value")
 
-    const selectorItem = props.items.find(i => i.value === props.value);
+
+    const [active, setActive] = useState(false);
+    const [hoveredElementValue, setHoveredElementValue] = useState(value);
+
+    const selectorItem = props.items.find(i => i.value === value);
     const hoveredItem = props.items.find(i => i.value === hoveredElementValue);
+    console.log(selectorItem)
 
     useEffect(() => (
-        setHoveredElementValue(props.value)
-    ), [props.value])
+        setHoveredElementValue(value)
+    ), [value])
 
     const toggleItems = () => setActive(!active);
     const onItemClick = (value: any) => {
-        props.onChange(value);
+        onChange(value);
         toggleItems()
     }
 
@@ -38,13 +46,13 @@ export function Select(props: SelectPropsType) {
                         : props.items[i - 1];
 
                     if (pretendentElement) {
-                        props.onChange(pretendentElement.value)
+                        onChange(pretendentElement.value)
                         return
                     }
                 }
             }
             if (!selectorItem) {
-                props.onChange(props.items[0].value);
+                onChange(props.items[0].value);
             }
         }
 
@@ -54,23 +62,26 @@ export function Select(props: SelectPropsType) {
     }
 
     return (
-        <>
+        <div >
             <div className={s.select} onKeyUp={onKeyUp} tabIndex={0}>
                 <span className={s.main} onClick={toggleItems}> {selectorItem && selectorItem.title} </span>
                 {
                     active &&
                     <div className={s.items}>
-                        {props.items.map(i => <div
+                        {props.items.map(i =>{
+
+
+                           return <div
                                 onMouseEnter={() => setHoveredElementValue(i.value)}
                                 className={s.item + " " + (hoveredItem === i ? s.selected : "")}
                                 key={i.value}
                                 onClick={() => onItemClick(i.value)}>
                                 {i.title}
                             </div>
-                        )}
+                        })}
                     </div>
                 }
             </div>
-        </>
+        </div>
     )
 }
